029b25a67b926e661ed877c8d9c10a8de040d289
DuskL1ghtness <114758274+DuskL1ghtness@users.noreply.github.com>
2026-07-15 12:30:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const windowEl = document.querySelector(".xp-window");
+const title = document.querySelector(".titlebar");
+
+let dragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+title.addEventListener("mousedown", e => {
+
+    dragging = true;
+
+    offsetX = e.clientX - windowEl.offsetLeft;
+    offsetY = e.clientY - windowEl.offsetTop;
+
+});
+
+document.addEventListener("mousemove", e => {
+
+    if(!dragging) return;
+
+    windowEl.style.left = (e.clientX - offsetX) + "px";
+    windowEl.style.top = (e.clientY - offsetY) + "px";
+
+});
+
+document.addEventListener("mouseup", ()=>{
+
+    dragging = false;
+
+});
